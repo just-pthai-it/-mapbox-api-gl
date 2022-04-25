f00d65daa48c71307038e75bd3ad7ac6524f6f05
WRBKOR23 <pthai.it.210501@gmail.com>
2022-04-25 08:28:12 +0700

    create suggestion input

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var addresses = [];
+
+
 const accessToken =
     'pk.eyJ1IjoicHRoYWktaXQtZGV2IiwiYSI6ImNsMmI4enZrMjBjd3UzZmxlcml5NW0ydW0ifQ.QuS_NNfgeHo62B-f-ZzCHA';
 let marker1 = new mapboxgl.Marker();
@@ -51,21 +54,10 @@ async function search() {
 
     let url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
         document.getElementById('start').value +
-        '.json?country=vn&proximity=ip&types=place,postcode,address&access_token=' + accessToken;
+        '.json?limit=6&country=vn&proximity=ip&types=place,postcode,address&access_token=' + accessToken;
     let responseJson = await fetchUrl(url, init);
 
-    marker1.remove();
-    marker1 = new mapboxgl.Marker({
-            draggable: true,
-        })
-        .setLngLat(responseJson.features[0].center)
-        .addTo(map);
-
-    map.flyTo({
-        center: responseJson.features[0].center
-    });
-    getRoute(responseJson.features[0].center, end)
-
+    addresses = responseJson.features;
     return responseJson;
 }
 
@@ -127,3 +119,121 @@ async function fetchUrl(url, init) {
 
     return responseJson;
 }
+
+function autocomplete(inp) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        search();
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) {
+            return false;
+        }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < addresses.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (addresses[i].place_name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + addresses[i].place_name.substr(0, val.length) + "</strong>";
+                b.innerHTML += addresses[i].place_name.substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + addresses[i].place_name + "' class='" + i + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    let index = this.getElementsByTagName("input")[0].classList[0];
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+
+                    marker1.remove();
+                    marker1 = new mapboxgl.Marker({
+                            draggable: true,
+                        })
+                        .setLngLat(addresses[index].center)
+                        .addTo(map);
+
+                    map.flyTo({
+                        center: addresses[index].center
+                    });
+                    getRoute(addresses[index].center, end)
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+autocomplete(document.getElementById("start"));
